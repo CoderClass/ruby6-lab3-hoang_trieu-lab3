2814600d94a429246e4a7eddc56e1e1d68dc95f4
phuonghoang713 <phuonghoang713@gmail.com>
2017-06-28 19:42:21 +0700

    skip milestone for error

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  RSpec.describe Product, type: :model do
+  # Use .alphabetical because it's a class method. Example: Product.alphabetical
+  # Use #long_name when #long_name is an instance method. Example: @product.long_name
+  describe ".alphabetical" do
+    it "returns [] when there are no products" do
+      # ...
+    end
+
+    it "returns [a] when there is only one product a" do
+      # a = Product.create!(...)
+      expect(...).to eq ...
+    end
+
+    it "returns [a,b,c] after I create 3 products c, b, a" do
+      # ...
+    end
+  end
 end
